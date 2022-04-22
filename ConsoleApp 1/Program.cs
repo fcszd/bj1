c6e124fc6b2104781c7079de141a43b93b8ebc0a
fcszd <104209385+fcszd@users.noreply.github.com>
2022-04-22 21:42:43 +0800

    Add files via upload

---
+++ ConsoleApp 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 1000.0;
+            double theta = 45.0;
+            Console.WriteLine("r={0:E};theta={1:F}", r, theta);
+            theta = theta * Math.PI / 180;
+            double x = r * Math.Sin(theta);
+            double y = r * Math.Cos(theta);
+            Console.WriteLine("x={0:0.000};y={1:0.000}", x, y);
+            Console.ReadKey();
+        }
+    }
+}
